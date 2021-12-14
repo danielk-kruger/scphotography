860e4b695967387e27103624ec120d1b7afd8257
danielk-kruger <kruger.dkk@gmail.com>
2021-12-14 11:57:57 +0200

    Performance fixes and reduced website load

--- js/main.js
+++ js/main.js
@@ -1,38 +1,68 @@
-window.onload = () => {
-  const masonry = new Masonry('.grid', {
-    itemSelector: '.grid-item',
-    gutter: 5,
-    horizontalOrder: true,
-    fitWidth: true,
-    transitionDuration: 0,
-  });
-
-  if (window.innerWidth >= 1024) {
-    masonry.gutter = 1;
-    masonry.horizontalOrder = false;
-  }
-};
-
-AOS.init({
-  useClassNames: false,
-  easing: 'ease-in-out',
-  duration: 400,
-  once: false,
-  offset: -205,
-  anchorPlacement: 'top-center',
-});
-
-if (window.innerWidth >= 1024) {
-  AOS.init({
-    offset: -210,
-  });
-}
-
 const navMenu = document.querySelector('.navbar-menu');
 const nav = document.querySelector('.navbar');
 const mobileMenu = document.getElementById('mobileMenu');
+const gridGallery = document.querySelector('.grid');
 
-mobileMenu.addEventListener('click', () => {
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: 'zoqrycqrv59z',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: '3PL04WHPgmI7ZwK8pTy1ERf8ecaOeBS8ERCrnK7XUQ4',
+});
+
+class Content {
+  async getGallery() {
+    try {
+      const galleryContent = await client.getEntries({
+        content_type: 'scPhotography',
+      });
+
+      let gallery = galleryContent.items;
+      // console.log(gallery);
+      gallery = gallery.map((item) => {
+        const { title, gallery } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        const type = item.fields.image.fields.title;
+        return { title, gallery, id, type, image };
+      });
+
+      // console.log(gallery.reverse());
+      return gallery.reverse();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+class UI {
+  displayGalleryContent(contentItems) {
+    let res = '';
+    contentItems.forEach((item) => {
+      res += `
+      <div class="grid-item ${item.type}" data-aos="fade-up">
+        <img src=${item.image} alt="" />
+      </div>
+      `;
+    });
+
+    gridGallery.innerHTML = res;
+  }
+
+  displayAboutContent() {}
+}
+
+class Storage {
+  static saveContent(content) {
+    localStorage.setItem('content', JSON.stringify(content));
+  }
+}
+
+// window.onload = () => {
+
+// };
+
+function hideMenu() {
   const icon = mobileMenu.querySelector('i');
   nav.classList.toggle('mobile');
 
@@ -43,11 +73,53 @@ mobileMenu.addEventListener('click', () => {
     icon.classList.remove('fa-times');
     icon.classList.add('fa-bars');
   }
+}
+
+mobileMenu.addEventListener('click', () => {
+  hideMenu();
 });
 
-const authorContent = document.querySelectorAll('.creator');
+const authorContent = document.querySelector('.authors').children;
 
 window.addEventListener('DOMContentLoaded', () => {
+  const content = new Content();
+  const ui = new UI();
+
+  content.getGallery().then((item) => {
+    ui.displayGalleryContent(item);
+    Storage.saveContent(item);
+
+    // Tile the Gallery Images
+    const masonry = new Masonry('.grid', {
+      itemSelector: '.grid-item',
+      gutter: 5,
+      horizontalOrder: true,
+      fitWidth: true,
+      transitionDuration: 0,
+    });
+
+    if (window.innerWidth >= 1024) {
+      masonry.gutter = 1;
+      masonry.horizontalOrder = false;
+    }
+  });
+
+  // Fading effects
+  AOS.init({
+    useClassNames: false,
+    easing: 'ease-in-out',
+    duration: 400,
+    once: false,
+    offset: -205,
+    anchorPlacement: 'top-center',
+  });
+
+  if (window.innerWidth >= 1024) {
+    AOS.init({
+      offset: -210,
+    });
+  }
+
   let index = 0;
   const slides = authorContent;
   const classHide = 'hide',
@@ -66,23 +138,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const mobileIsActive = document.querySelector('.navbar.mobile');
 
     if (window.innerWidth <= 820 && mobileIsActive) {
-      const icon = mobileMenu.querySelector('i');
-      nav.classList.toggle('mobile');
-
-      if (nav.classList.contains('mobile')) {
-        icon.classList.remove('fa-bars');
-        icon.classList.add('fa-times');
-      } else {
-        icon.classList.remove('fa-times');
-        icon.classList.add('fa-bars');
-      }
-    }
-
-    // document.querySelector('#about').scrollIntoView(true);
-
-    function scrollToAbout(e) {
-      // e.preventDefault();
-      const about = document.getElementById('#about').scrollIntoView(false);
+      hideMenu();
     }
   });
 });
