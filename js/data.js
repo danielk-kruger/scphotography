a7fda245e942988cbea19731b845e979553bde07
danielk-kruger <kruger.dkk@gmail.com>
2022-01-04 20:49:43 +0200

    Added dynamic information

---
+++ js/data.js
@@ -0,0 +1,57 @@
+export const FAQs = [
+  {
+    question: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit?',
+    ans: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse  commodi saepe eveniet quos voluptas incidunt ratione aliquam nisi cum culpa.',
+    id: 0,
+  },
+  {
+    question:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,sint?',
+    ans: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse commodi saepe eveniet quos voluptas',
+    id: 1,
+  },
+  {
+    question: 'amet consectetur adipisicing elit?',
+    ans: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse commodi saepe eveniet quos voluptas incidunt ratione',
+    id: 2,
+  },
+  {
+    question:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius quis debitis fuga recusandae ad nisi error earum dicta, necessitatibus delectus deleniti eaque, tempore alias totam, inventore omnis sed ipsam sit?',
+    ans: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique quidem ipsum accusantium et ut sequi, veniam possimus quod quam beatae.',
+    id: 3,
+  },
+  {
+    question: 'Lorem ipsum dolor sit elit?',
+    ans: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse commodi saepe eveniet quos voluptas .',
+    id: 4,
+  },
+];
+
+export const userRevs = [
+  {
+    name: 'Charlie Heyneke',
+    com: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta nesciunt ut quam molestias corrupti, corporis suscipit possimus nobis earum!',
+    profile: '../images/charlie-chaplin.png',
+  },
+  {
+    name: 'Michael Jackson',
+    com: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta nesciunt ut quam molestias corrupti, corporis suscipit possimus nobis earum!',
+    profile: '../images/img1.jpg',
+  },
+  {
+    name: 'Master Chelly',
+    com: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta nesciunt ut quam molestias corrupti, corporis suscipit possimus nobis earum!',
+    profile: '../images/chelly.jpeg',
+  },
+  {
+    name: 'Renee Lahn',
+    com: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta nesciunt ut quam molestias corrupti, corporis suscipit possimus nobis earum!',
+    profile: '../images/img15.jpg',
+  },
+  {
+    name: 'Lauren James',
+    com: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt soluta nesciunt ut quam molestias corrupti, corporis suscipit possimus nobis earum!',
+    profile: '../images/img8.jpg',
+  },
+];
--- js/main.js
+++ js/main.js
@@ -5,17 +5,27 @@ const gridGallery = document.querySelector('.grid');
 const authorContent = document.querySelectorAll('.creator');
 const aboutContent = document.querySelector('.authors');
 
+const uploadBtn = document.getElementById('upload');
+const commentField = document.getElementById('user-description');
+const nameField = document.querySelector('.user-name__field');
+const mailField = document.querySelector('.user-email__field');
+const submitReviewBtn = document.querySelector('.user-send__msg');
+
+import { FAQs, userRevs } from './data.js';
+
 $(document).ready(function () {
-  $('.user-review__submit').on('click', function () {
+  // let maxReviews = 5;
+
+  $('.user-review__submit').on('click', () => {
     $('.user-review__form').addClass('active');
 
-    $('.user-review__form').fadeIn('fast', function () {
+    $('.user-review__form').fadeIn('fast', () => {
       $('#form-field').fadeIn('fast');
     });
   });
 
   $('.close-popup').on('click', function () {
-    $('.user-review__form').fadeOut('fast', function () {
+    $('.user-review__form').fadeOut('fast', () => {
       $('#form-field').fadeOut('fast');
       $('.user-review__form').removeClass('active');
     });
@@ -28,7 +38,7 @@ $(document).ready(function () {
       if ($(event.target).next().hasClass('hide')) {
         $(event.target)
           .next()
-          .slideDown(300, function () {
+          .slideDown(300, () => {
             $(event.target).next().removeClass('hide');
           });
         $('.last').css('border-radius', '0');
@@ -40,7 +50,7 @@ $(document).ready(function () {
       } else {
         $(event.target)
           .next()
-          .slideUp(300, function () {
+          .slideUp(300, () => {
             $(event.target).next().addClass('hide');
           });
         $('.last').css('border-radius', '');
@@ -59,7 +69,7 @@ $(document).ready(function () {
     $('.faq-toggle').toggleClass('selected');
   });
 
-  sliderSettings = {
+  let sliderSettings = {
     item: 10,
     cssEasing: 'cubic-bezier(0.35, 0, 0.35, 1)',
     easing: 'cubic-bezier(0.35, 0, 0.35, 1)',
@@ -89,6 +99,102 @@ $(document).ready(function () {
   $('#light-slider-2').lightSlider(sliderSettings);
 });
 
+function handleUploadedImage() {
+  uploadBtn.addEventListener('change', () => {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const upload = reader.result;
+      localStorage.setItem('uploadData', upload);
+    });
+
+    reader.readAsDataURL(uploadBtn.files[0]);
+  });
+}
+
+function addUserReview(revArr) {
+  handleUploadedImage();
+
+  submitReviewBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const fetchImage = localStorage.getItem('uploadData');
+    revArr.map((revs) => {
+      return {
+        ...revs,
+        name: nameField.value,
+        email: mailField.value,
+        com: commentField.value,
+        profile: fetchImage,
+      };
+    });
+
+    localStorage.setItem('userReviews', JSON.stringify(revs));
+    console.log(userRevs);
+  });
+}
+
+function updateReviews(revArr) {
+  addUserReview(revArr);
+
+  for (let rev of revArr) {
+    let userR = `
+    <div class="reviews-card">
+      <div class="reviews-card__profile">
+        <div class="profile-title">
+          <h3>${rev.name}</h3>
+        </div>
+        <div class="profile-image">
+          <img src="${rev.profile}" alt="" />
+        </div>
+      </div>
+      <div class="reviews-card__body">
+        <i class="fas fa-quote-left"></i>
+        <p>
+          ${rev.com}
+        </p>
+      </div>
+    </div>
+    `;
+
+    document.querySelector('.reviews').insertAdjacentHTML('beforeend', userR);
+  }
+}
+
+function updateFAQs(faqArr) {
+  for (let item of faqArr) {
+    let userQ = `
+      <div class="faq-content">
+        <div class="faq-content__title faq-toggle ${
+          item.id === 0 ? 'first' : ''
+        } ${item.id === 4 ? 'last' : ''}">
+          <span href="#" class="faq-content__title--text"
+            >${item.question}</span
+          >
+          <i class="fas fa-plus"></i>
+        </div>
+        <div class="faq-content__acc hide">
+          <div class="faq-content__acc--info">
+            <p>
+              ${item.ans}
+            </p>
+
+            <div class="ans-rating">
+              <a href="#">Was this answer useful? </a>
+              <i class="fas fa-thumbs-up"></i>
+              <i class="fas fa-thumbs-down"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    document
+      .querySelector('.faq-container')
+      .insertAdjacentHTML('beforeend', userQ);
+  }
+}
+
 function lockScrolling() {
   document.querySelector('body').style.overflow = 'hidden';
 }
@@ -117,6 +223,9 @@ mobileMenu.addEventListener('click', () => {
 });
 
 window.addEventListener('DOMContentLoaded', () => {
+  updateReviews(userRevs);
+  updateFAQs(FAQs);
+
   AOS.init({
     useClassNames: false,
     easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
